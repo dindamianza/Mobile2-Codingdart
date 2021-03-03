83347f1ec95e98307b6e23f1358db5d0dea40e33
dindamianza <79966214+dindamianza@users.noreply.github.com>
2021-03-03 01:18:40 -0800

    Add files via upload

---
+++ variabel.dart
@@ -0,0 +1,15 @@
+void main(){
+    // membaut variabel dengan tipe data
+    String nama = "Dinda Mianza";
+    int umur = 19;
+    double tinggi = 160;
+    bool isMenikah = false;
+
+    // membuat veriabel dengan kata kunci var
+    var alamat = "Medan, Indonesia";
+
+    // mencetak variabel
+    print("Nama saya $nama. Umur $umur tahun. Tinggi sekitar $tinggi cm.");
+    print("Menikah: $isMenikah");
+    print("Alamat: $alamat");
+}
